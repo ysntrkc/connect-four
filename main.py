2dd474c7b2fbdc71799384bbcaff223d8a29a8bd
yasin.tarakci <yasintarakci42@gmail.com>
2023-01-15 00:57:53 +0300

    make a terminal ui and add new heuristic

--- main.py
+++ main.py
@@ -3,10 +3,91 @@ import game
 import negamax as ngmx
 
 
-def main():
-	board = [[' ']*8 for i in range(7)]
+def game_ui(board):
+	print('Welcome to Connect 4!')
+	while True:
+		print('Choose a game option:')
+		print('\t1. Player vs Player\n\t2. Player vs AI\n\t3. AI vs AI\n\t4. Exit')
+		try:
+			game_option = int(input('Option: '))
+		except KeyboardInterrupt:
+			break
+		except:
+			print('Invalid option, try again.')
+			continue
+		if game_option == 1:
+			player_vs_player(board)
+			break
+		elif game_option == 2:
+			try:
+				heuristic_num = int(input('Choose a heuristic (1 or 2): '))
+			except:
+				print('Invalid option, try again.')
+				continue
+			player_vs_ai(board, heuristic_num)
+			break
+		elif game_option == 3:
+			try:
+				heuristic_num1 = int(input('Choose a heuristic for 1st AI (1 or 2): '))
+				heuristic_num2 = int(input('Choose a heuristic for 2nd AI (1 or 2): '))
+			except:
+				print('Invalid option, try again.')
+				continue
+			ai_vs_ai(board, heuristic_num1, heuristic_num2)
+			break
+		elif game_option == 4:
+			break
+		else:
+			print('Invalid option, try again.')
 
-	game.print_board(board)
+
+def player_vs_player(board):
+	turn = 'X'
+	while True:
+		if turn == 'X':
+			try:
+				col = int(input(f'Player {turn}, choose your column (1-8): '))
+			except KeyboardInterrupt:
+				break
+			except:
+				print('Invalid move, try again.')
+				continue
+			if game.is_valid_move(board, col):
+				board = game.make_move(board, col, turn)
+				if game.is_winner(board, turn):
+					game.print_board(board)
+					print(f'Player {turn} wins!')
+					break
+				turn = 'O' if turn == 'X' else 'X'
+				game.print_board(board)
+			else:
+				print('Invalid move, try again.')
+			if game.is_board_full(board):
+				print('The game is a tie!')
+				break
+		else:
+			try:
+				col = int(input(f'Player {turn}, choose your column (1-8): '))
+			except KeyboardInterrupt:
+				break
+			except:
+				print('Invalid move, try again.')
+				continue
+			if game.is_valid_move(board, col):
+				board = game.make_move(board, col, turn)
+				if game.is_winner(board, turn):
+					game.print_board(board)
+					print(f'Player {turn} wins!')
+					break
+				turn = 'O' if turn == 'X' else 'X'
+				game.print_board(board)
+			else:
+				print('Invalid move, try again.')
+			if game.is_board_full(board):
+				print('The game is a tie!')
+				break
+
+def player_vs_ai(board, hueristic_num):
 	turn = 'X'
 	while True:
 		if turn == 'X':
@@ -32,7 +113,7 @@ def main():
 				break
 		else:
 			state = [row.copy() for row in board]
-			column, score = ngmx.negamax(state=state, depth=0, max_depth=5, isMaximizer=True, alpha=-math.inf, beta=math.inf)
+			column, score = ngmx.negamax(state=state, depth=0, max_depth=5, isMaximizer=True, alpha=-math.inf, beta=math.inf, heuristic_num=heueristic_num)
 			if game.is_valid_move(board, column):
 				board = game.make_move(board, column, turn)
 
@@ -47,6 +128,48 @@ def main():
 				if game.is_board_full(board):
 					print('The game is a tie!')
 					break
+
+def ai_vs_ai(board, hueristic_num_1, hueristic_num_2):
+	turn = 'X'
+	while True:
+		if turn == 'X':
+			state = [row.copy() for row in board]
+			column, score = ngmx.negamax(state=state, depth=0, max_depth=4, isMaximizer=True, alpha=-math.inf, beta=math.inf, heuristic_num=hueristic_num_1)
+			if game.is_valid_move(board, column):
+				board = game.make_move(board, column, turn)
+
+				if game.is_winner(board, turn):
+					game.print_board(board)
+					print(f'AI {turn} wins!')
+					break
+
+				turn = 'O' if turn == 'X' else 'X'
+				game.print_board(board)
+
+				if game.is_board_full(board):
+					print('The game is a tie!')
+					break
+		else:
+			state = [row.copy() for row in board]
+			column, score = ngmx.negamax(state=state, depth=0, max_depth=8, isMaximizer=True, alpha=-math.inf, beta=math.inf, heuristic_num=hueristic_num_2)
+			if game.is_valid_move(board, column):
+				board = game.make_move(board, column, turn)
+
+				if game.is_winner(board, turn):
+					game.print_board(board)
+					print(f'AI {turn} wins!')
+					break
+
+				turn = 'O' if turn == 'X' else 'X'
+				game.print_board(board)
+
+				if game.is_board_full(board):
+					print('The game is a tie!')
+					break
+
+def main():
+	board = [[' ']*8 for i in range(7)]
+	game_ui(board)
 
 if __name__ == '__main__':
 	main()
--- negamax.py
+++ negamax.py
@@ -1,7 +1,7 @@
 import math
 import game
 
-def calculate_sequence(sequence, coin, isDiagonal):
+def calculate_sequence_heuristic_1(sequence, coin, isDiagonal):
     if coin == 'X':
         opponent = 'O'
     else:
@@ -32,8 +32,39 @@ def calculate_sequence(sequence, coin, isDiagonal):
 
     return utility
 
+def calculate_sequence_heuristic_2(sequence, coin, isDiagonal, col):
+    if coin == 'X':
+        opponent = 'O'
+    else:
+        opponent = 'X'
 
-def utility_score(state, isMaximizer):
+    utility = 2 - ((((col - 3.5) ** 2)) / 6.125)
+
+    if sequence.count(coin) == 4:
+        utility += 4 + isDiagonal
+
+    elif sequence.count(coin) == 3 and sequence.count(' ') == 1:
+        utility += 3 + isDiagonal
+
+    elif sequence.count(coin) == 2 and sequence.count(' ') == 2:
+        utility += 2 + isDiagonal
+
+    elif sequence.count(coin) == 1 and sequence.count(' ') == 3:
+        utility += 1 + isDiagonal
+
+    if sequence.count(opponent) == 3 and sequence.count(' ') == 1:
+        utility -= 15
+
+    elif sequence.count(opponent) == 2 and sequence.count(' ') == 2:
+        utility -= 4
+
+    elif sequence.count(opponent) == 1 and sequence.count(' ') == 3:
+        utility -= 1
+
+    return utility
+
+
+def utility_score(state, isMaximizer, heuristic_num):
     if isMaximizer:
         coin = 'X'
     else:
@@ -46,29 +77,41 @@ def utility_score(state, isMaximizer):
         column_array = [state[r][c] for r in range(7)]
         for r in range(4):
             sequence = column_array[r:r+4]
-            utility += calculate_sequence(sequence, coin, 0)
+            if heuristic_num == 1:
+                utility += calculate_sequence_heuristic_1(sequence, coin, 0)
+            elif heuristic_num == 2:
+                utility += calculate_sequence_heuristic_2(sequence, coin, 0, c)
 
     # Check for horizontal score.
     for r in range(7):
         row_array = [state[r][c] for c in range(8)]
         for c in range(5):
             sequence = row_array[c:c+4]
-            utility += calculate_sequence(sequence, coin, 0)
+            if heuristic_num == 1:
+                utility += calculate_sequence_heuristic_1(sequence, coin, 0)
+            elif heuristic_num == 2:
+                utility += calculate_sequence_heuristic_2(sequence, coin, 0, c)
 
     for r in range(4):
         for c in range(5):
             sequence = [state[r+i][c+i] for i in range(4)]
-            utility += calculate_sequence(sequence, coin, 1)
+            if heuristic_num == 1:
+                utility += calculate_sequence_heuristic_1(sequence, coin, 1)
+            elif heuristic_num == 2:
+                utility += calculate_sequence_heuristic_2(sequence, coin, 1, c)
 
     for r in range(4):
         for c in range(5):
             sequence = [state[r+3-i][c+i] for i in range(4)]
-            utility += calculate_sequence(sequence, coin, 1)
+            if heuristic_num == 1:
+                utility += calculate_sequence_heuristic_1(sequence, coin, 1)
+            elif heuristic_num == 2:
+                utility += calculate_sequence_heuristic_2(sequence, coin, 1, c)
 
     return utility
 
 
-def negamax(state, depth, max_depth, isMaximizer, alpha, beta):
+def negamax(state, depth, max_depth, isMaximizer, alpha, beta, heuristic_num):
     if depth == max_depth or game.is_winner(state, 'X') or game.is_winner(state, 'O') or game.is_board_full(state):
         if game.is_winner(state, 'X'):
             return [-1, math.inf]
@@ -77,7 +120,7 @@ def negamax(state, depth, max_depth, isMaximizer, alpha, beta):
         elif game.is_board_full(state):
             return [-1, 0]
         else:
-            return [-1, utility_score(state, isMaximizer)]
+            return [-1, utility_score(state, isMaximizer, heuristic_num)]
 
     if isMaximizer == True:
         value = -math.inf
@@ -86,7 +129,7 @@ def negamax(state, depth, max_depth, isMaximizer, alpha, beta):
         for column in moves:
             copied_state = [row.copy() for row in state]
             game.make_move(copied_state, column, 'X')
-            score = negamax(copied_state, depth+1, max_depth, False, alpha, beta)[1]
+            score = negamax(copied_state, depth+1, max_depth, False, alpha, beta, heuristic_num)[1]
             if score > value:
                 value = score
                 best_column = column
@@ -102,7 +145,7 @@ def negamax(state, depth, max_depth, isMaximizer, alpha, beta):
         for column in moves:
             copied_state = [row.copy() for row in state]
             game.make_move(copied_state, column, 'O')
-            score = negamax(copied_state, depth+1,max_depth, True, alpha, beta)[0]
+            score = negamax(copied_state, depth+1,max_depth, True, alpha, beta, heuristic_num)[0]
             if score < value:
                 value = score
                 best_column = column
